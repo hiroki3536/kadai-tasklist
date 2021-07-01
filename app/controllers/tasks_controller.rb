24f4e47bb5688e5958e9a6389fc35a159c415fdd
hiroki <hiroki15448@gmail.com>
2021-07-01 09:55:29 +0000

    fix

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -15,10 +15,10 @@ class TasksController < ApplicationController
     @task = Task.new(task_params)
     
     if @task.save
-      flash[:success] = "Task が正常に作成されました"
+      flash[:success] = "Task が正常に投稿されました"
       redirect_to @task
     else
-      flash.now[:danger] = "Task が作成されませんでした"
+      flash.now[:danger] = "Task が投稿されませんでした"
       render :new 
     end
   end
